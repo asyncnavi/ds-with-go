0cfd4213e3cbc9d3d75af9714ab05b294b7d34da
Navraj Sandhu <asyncnavi@gmail.com>
2024-04-07 11:35:29 +0530

    day 5: doubly linked list

---
+++ linked_list/doubly.go
@@ -0,0 +1,38 @@
+package linked_list
+
+import "fmt"
+
+type DNode struct {
+	prev *DNode
+	data int
+	next *DNode
+}
+
+type DList struct {
+	head *DNode
+}
+
+func (list *DList) PrintList() {
+	cursor := list.head
+
+	for cursor != nil {
+		fmt.Printf("%d ,", cursor.data)
+		cursor = cursor.next
+	}
+}
+
+func (list *DList) InsertAtEnd(data int) {
+
+	if list.head == nil {
+		list.head = &DNode{prev: nil, data: data, next: nil}
+		return
+	}
+
+	cursor := list.head
+
+	for cursor.next != nil {
+		cursor = cursor.next
+	}
+	newNode := &DNode{prev: cursor, data: data, next: nil}
+	cursor.next = newNode
+}
--- main.go
+++ main.go
@@ -2,25 +2,17 @@ package main
 
 import (
 	"ds-go/linked_list"
-	"fmt"
 )
 
 func main() {
-	list := &linked_list.CList{}
-	list.PushEnd(5)
-	list.PushEnd(6)
-	list.PushEnd(7)
-	list.PushEnd(8)
-	list.PushEnd(4)
-	list.PushEnd(10)
+	list := &linked_list.DList{}
+	list.InsertAtEnd(5)
+	list.InsertAtEnd(6)
+	list.InsertAtEnd(7)
+	list.InsertAtEnd(8)
+	list.InsertAtEnd(4)
+	list.InsertAtEnd(10)
 
 	list.PrintList()
-	firstMid, secondMid, isSecondNull := list.GetMid()
-
-	if isSecondNull {
-		fmt.Println("\nMiddle node of the list is :", firstMid)
-	} else {
-		fmt.Println("\nSince the list has even number of node so it has two mid: ", firstMid, secondMid)
-	}
 
 }
