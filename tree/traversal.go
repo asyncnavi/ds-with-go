7fc79ccfa2bff86dd05c7add04dbdb2424142192
Navraj Sandhu <asyncnavi@gmail.com>
2024-05-05 12:46:59 +0530

    add Preorder Interative

--- tree/traversal.go
+++ tree/traversal.go
@@ -1,6 +1,33 @@
 package tree
 
-import "fmt"
+import (
+	"ds-go/stack"
+	"fmt"
+)
+
+func PreOrder(root *Node) {
+	if root == nil {
+		return
+	}
+
+	st := &stack.LStack{}
+	st.New()
+
+	st.Push(root)
+
+	for !st.IsEmpty() {
+		node := st.Pop().(*Node)
+		fmt.Printf("%d ", node.data)
+
+		// Push the right child first so that it gets processed after the left child
+		if node.right != nil {
+			st.Push(node.right)
+		}
+		if node.left != nil {
+			st.Push(node.left)
+		}
+	}
+}
 
 func PreOrderR(root *Node) {
 	if root != nil {
